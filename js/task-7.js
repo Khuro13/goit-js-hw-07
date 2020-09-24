f9c9740c526b28776d91d30a244c28ff2a7c98f1
Ivan Dobrolyubov <66012866+Khuro13@users.noreply.github.com>
2020-09-24 21:27:58 +0300

    end

---
+++ js/task-7.js
@@ -0,0 +1,6 @@
+let controlFont = document.getElementById("font-size-control");
+let text = document.getElementById("text");
+
+controlFont.oninput = function () {
+  text.style.fontSize = controlFont.value + "px";
+};
